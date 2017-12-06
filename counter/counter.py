ad5ce718046bf767b0ae7db6b8c1a6e8b853110f
Kavita Amin <kavita.ux@gmail.com>
2017-12-06 08:20:27 -0500

    first commit

---
+++ counter/counter.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, session
+app = Flask(__name__)
+app.secret_key = 'SecretKey'
+
+
+@app.route('/')
+def index(): 
+    if "counter" not in session: 
+        session["counter"] = 0
+    session["counter"]+=1
+    return render_template("index.html", counter=session["counter"])
+
+app.run(debug=True)
