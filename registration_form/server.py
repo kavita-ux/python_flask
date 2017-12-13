2b19940b5f179fa6d356c653411dc939c11eab1d
Kavita Amin <33613805+kavita-ux@users.noreply.github.com>
2017-12-12 21:55:19 -0500

    first commit

---
+++ registration_form/server.py
@@ -0,0 +1,68 @@
+"""
+Kavita Amin
+12/3/2017
+Registration Form: Validations and Flashing Errors
+All fields are required and must not be blank
+First and Last Name cannot contain any numbers
+Password should be more than 8 characters
+Email should be a valid email
+Password and Password Confirmation should match
+"""
+
+
+from flask import Flask, render_template, redirect, flash, request
+import re 
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+app = Flask(__name__)
+app.secret_key = "Secret"
+
+@app.route('/')
+def index(): 
+    return render_template("index.html")
+
+@app.route('/register', methods=["POST"])
+def register(): 
+    isError = False
+
+    # no fields left empty
+    if len(request.form["firstname"]) < 1: 
+        flash("First Name is required.")
+        isError = True
+    if len(request.form["lastname"]) < 1: 
+        flash("Last Name is required.")
+        isError = True
+    if len(request.form["email"]) < 1: 
+        flash("Email is required.")
+        isError = True
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!")    
+        isError = True    
+    if len(request.form["password"]) < 9: 
+        flash("Password is required and should be more than 8 characters.")
+        isError = True
+    if len(request.form["firstname"]) < 1: 
+        flash("Please confirm your password.")
+        isError = True
+
+    # checking if name contains numbers
+    if request.form["firstname"].isalpha() == False: 
+        flash("First name should not contain any numbers.")
+        isError = True
+    if request.form["lastname"].isalpha() == False: 
+        flash("Last name should not contain any numbers.")
+        isError = True
+
+    # password fields should match 
+    if (request.form["password"]) != (request.form["password_confirm"]): 
+        flash("Passwords must match.")
+        isError = True
+
+    if isError == True: 
+        return redirect('/')
+    else: 
+        flash("Thanks for submitting your information.")
+        return redirect('/')    
+
+
+app.run(debug=True)
